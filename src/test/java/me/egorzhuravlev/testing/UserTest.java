66fde5eccb297c79a9ffacbde49eb61b8a2499f5
Icedragal <icedragal@gmail.com>
2023-02-11 15:44:25 +0300

    Выполнил дз

---
+++ src/test/java/me/egorzhuravlev/testing/UserTest.java
@@ -0,0 +1,45 @@
+package me.egorzhuravlev.testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    public void editUserWithTwoParamTest() {
+        String login = "user";
+        String email = "user@mail.ru";
+        User user = new User(login, email);
+
+        assertNotNull(user.getLogin(), user.getEmail());
+    }
+
+    @Test
+    public void editUserWithoutParamTest() {
+        User user = new User();
+
+        assertNull(user.getLogin(), user.getEmail());
+    }
+
+    @Test
+    public void checkCorrectnessUsersEmailTest() {
+        String login = "user";
+        String email = "user@mail.ru";
+        User user = new User(login, email);
+        boolean check;
+        check = user.getEmail().contains("@") && user.getEmail().contains(".");
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void checkEqualsEmailAndLoginTest() {
+        String login = "user";
+        String email = "user@mail.ru";
+        User user = new User(login, email);
+
+        assertNotEquals(user.getLogin(), user.getEmail());
+
+    }
+}
+
