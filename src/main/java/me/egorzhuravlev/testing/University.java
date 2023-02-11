15ca2c7ec74de805a9006a9f8b2dc9b70d0c1b4d
Icedragal <icedragal@gmail.com>
2023-02-11 15:44:25 +0300

    Выполнил дз

---
+++ src/main/java/me/egorzhuravlev/testing/University.java
@@ -0,0 +1,34 @@
+package me.egorzhuravlev.testing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class University {
+    private Map<Integer, Student> allStudents = new HashMap<>();
+    private int countId;
+
+    public void addStudents(Student student) {
+        if (allStudents == null) {
+            allStudents = new HashMap<>();
+        }
+        student.setId(countId);
+        allStudents.put(countId, student);
+        countId++;
+    }
+
+    public List<Student> getAllStudents() {
+        return new ArrayList<>(allStudents.values());
+    }
+
+    public List<Student> getStudents(boolean isMale) {
+        List<Student> listStudents = new ArrayList<>();
+        for (Student student : allStudents.values()) {
+            if (student.isMale() == isMale) {
+                listStudents.add(student);
+            }
+        }
+        return listStudents;
+    }
+}
